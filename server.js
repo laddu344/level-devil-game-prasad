3e9a210ee5ed80eee0aa457dcc6af2eddc18897a
laddu344 <valmiki.9398@gmail.com>
2025-11-04 00:53:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "level-devil-game", "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "level-devil-game", "public", "index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
