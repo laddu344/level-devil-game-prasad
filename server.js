156e58743c9ebf82938be2f17bc3187a7a345dc2
laddu344 <valmiki.9398@gmail.com>
2025-11-04 01:15:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "level-devil-game", "public")));
+// ✅ Correct static path
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "level-devil-game", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
